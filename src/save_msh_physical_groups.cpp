8751e9ff9d2f8d4722b2007e4ee64dd3736541bc
Max Orok <maxwellorok@gmail.com>
2021-02-09 11:59:11 -0500

    Add support for saving physical groups

---
+++ src/save_msh_physical_groups.cpp
@@ -0,0 +1,25 @@
+#include "save_msh_physical_groups.h"
+
+#include <MshIO/MshSpec.h>
+#include <MshIO/exception.h>
+
+#include <cassert>
+#include <iomanip>
+
+namespace mshio {
+
+void save_physical_groups(std::ostream& out, const MshSpec& spec)
+{
+    out << "$PhysicalNames" << std::endl;
+    const auto& groups = spec.physical_groups;
+    out << groups.size() << std::endl;
+
+    for (const auto& group: groups) {
+        out << group.dim << " ";
+        out << group.tag << " ";
+        out << std::quoted(group.name) << std::endl;
+    }
+    out << "$EndPhysicalNames" << std::endl;
+}
+
+} // namespace mshio

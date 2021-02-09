8b14760f88c2ad6a1fcf4f93084951ff8dda0396
Max Orok <maxwellorok@gmail.com>
2021-02-09 11:30:23 -0500

    Add support for loading physical groups

---
+++ src/load_msh_physical_groups.cpp
@@ -0,0 +1,27 @@
+#include "load_msh_physical_groups.h"
+
+#include <MshIO/MshSpec.h>
+
+#include <cassert>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+namespace mshio {
+
+void load_physical_groups(std::istream& in, MshSpec& spec)
+{
+    auto& groups = spec.physical_groups;
+    int num_groups;
+    in >> num_groups;
+    spec.physical_groups.resize(num_groups);
+    for (int i = 0; i < num_groups; i++) {
+        auto& group = groups[i];
+        in >> group.dim;
+        in >> group.tag;
+        in >> std::quoted(group.name);
+    }
+    assert(in.good());
+}
+
+} // namespace mshio
---
+++ src/load_msh_physical_groups.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <MshIO/MshSpec.h>
+#include <iostream>
+
+namespace mshio {
+
+void load_physical_groups(std::istream& in, MshSpec& spec);
+
+} // namespace mshio

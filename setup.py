2a897f96ee76497d90dd8560325c232278aaea9d
Qingnan Zhou <qzhou@adobe.com>
2022-10-12 14:39:42 -0400

    Update python setup. (#27)

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ import sys, re, os
 try:
     from skbuild import setup
 except ImportError:
-    print("The preferred way to invoke 'setup.py' is via pip, as in 'pip "
-          "install .'. If you wish to run the setup script directly, you must "
-          "first install the build dependencies listed in pyproject.toml!",
-          file=sys.stderr)
+    print(
+        "The preferred way to invoke 'setup.py' is via pip, as in 'pip "
+        "install .'. If you wish to run the setup script directly, you must "
+        "first install the build dependencies listed in pyproject.toml!",
+        file=sys.stderr,
+    )
     raise
 
 setup(
@@ -17,9 +19,16 @@ setup(
     description="A tiny C++ library to read/write ASCII/binary MSH format files.",
     url="https://github.com/qnzhou/MshIO",
     license="Apache-2.0",
-    packages=['mshio'],
-    package_dir={'': 'python'},
+    packages=["mshio"],
+    package_dir={"": "python"},
     cmake_install_dir="python/mshio",
     include_package_data=True,
-    python_requires=">=3.8"
+    cmake_args=[
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DMSHIO_BUILD_TESTS=Off",
+        "-DMSHIO_BUILD_EXAMPLES=Off",
+        "-DMSHIO_EXT_NANOSPLINE=Off",
+        "-DMSHIO_PYTHON=On",
+    ],
+    python_requires=">=3.8",
 )

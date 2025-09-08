e56ca9bd6c78ad3cfb2c0b40bc4785ebf29a9b11
Qingnan Zhou <qzhou@adobe.com>
2025-09-08 10:56:18 -0400

    Update dependencies.

--- cmake/nanobind.cmake
+++ cmake/nanobind.cmake
@@ -1,13 +1,23 @@
-if (COMMAND nanobind_add_module)
+if(COMMAND nanobind_add_module)
     return()
 endif()
 
-include(cmake/python.cmake)
-FetchContent_Declare(
-    nanobind
-    GIT_REPOSITORY https://github.com/wjakob/nanobind.git
-    GIT_TAG v2.2.0
-    GIT_SHALLOW OFF)
+message(STATUS "Third-party (external): creating target 'nanobind::nanobind'")
 
-FetchContent_Populate(nanobind)
+include(CPM)
+CPMAddPackage(
+    NAME nanobind
+    GITHUB_REPOSITORY wjakob/nanobind
+    GIT_TAG v2.9.2
+    DOWNLOAD_ONLY ON
+)
+
+include(python)
+set(NB_SHARED OFF CACHE INTERNAL "")
+
+# Set this until https://github.com/wjakob/nanobind/issues/1123 is resolved.
+set(NB_PY_PATH "${Python_EXECUTABLE}" CACHE INTERNAL "Path to Python executable for nanobind")
+
+# Note that we do not use FetchContent_MakeAvailable here because nanobind's cmake changes
+# CMAKE_CXX_FLAGS and attempts to refind python, which can leads to cmake error.
 find_package(nanobind PATHS ${nanobind_SOURCE_DIR}/cmake NO_DEFAULT_PATH)
--- cmake/sanitizer-cmake.cmake
+++ cmake/sanitizer-cmake.cmake
@@ -1,16 +1,18 @@
-include_guard()
+if(COMMAND add_sanitizers)
+    return()
+endif()
 
-FetchContent_Declare(
-    sanitizer
-    GIT_REPOSITORY https://github.com/arsenm/sanitizers-cmake.git
-    GIT_TAG        99e159ec9bc8dd362b08d18436bd40ff0648417b
+message(STATUS "Third-party (external): creating method 'add_sanitizers'")
+
+include(CPM)
+CPMAddPackage(
+    NAME sanitizer
+    GITHUB_REPOSITORY arsenm/sanitizers-cmake
+    GIT_TAG 0573e2ea8651b9bb3083f193c41eb086497cc80a
+    DOWNLOAD_ONLY ON
 )
 
-FetchContent_GetProperties(sanitizer)
-if(NOT sanitizer_POPULATED)
-    FetchContent_Populate(sanitizer)
-    set(CMAKE_MODULE_PATH
-        "${sanitizer_SOURCE_DIR}/cmake"
-        ${CMAKE_MODULE_PATH})
-    find_package(Sanitizers REQUIRED)
-endif()
+block()
+set(CMAKE_MODULE_PATH "${sanitizer_SOURCE_DIR}/cmake" ${CMAKE_MODULE_PATH})
+find_package(Sanitizers REQUIRED)
+endblock()
